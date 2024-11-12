06953e68c5824a38042264520b051ae762efcec5
GG-Pharaoh <oxdiablo20@gmail.com>
2024-11-12 03:33:23 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const style = {
+  bg: `h-screen w-screen p-4 bg-gradient-to-r from-[#2F80ED] to-[#1CB5E0]`,
+}
+
+function App() {
+  
+
+  return (
+   <div className={style.bg}>
+
+   </div>
+  )
+}
+
+export default App
